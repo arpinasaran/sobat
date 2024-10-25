5961709a7dcf8c0c789f943bbabcdc0be5bf2877
JanssenBenedict <janssen.benedict@gmail.com>
2024-10-25 12:09:45 +0700

    Fixed Review View and Template Implementation

--- review/urls.py
+++ review/urls.py
@@ -5,9 +5,8 @@ app_name = 'review'
 
 urlpatterns = [
     path('', reviews, name='reviews'),
-    path('<uuid:product_id>/', reviews, name='reviews'),
-    path('<uuid:product_id>/json/', reviews_json, name='reviews_json'),
-    path('<uuid:product_id>/create/', create_review, name='create_review'),
-    path('<uuid:product_id>/create_ajax/', create_review_ajax, name='create_review_ajax'),
-    path('<uuid:review_id>/edit/', edit_review, name='edit_review'),
+    path('<str:product_id>/', reviews, name='reviews'),
+    path('<str:product_id>/create/', create_review, name='create_review'),
+    path('<str:product_id>/<str:review_id>/edit/', edit_review, name='edit_review'),
+    path('<str:product_id>/<str:review_id>/delete/', delete_review, name='delete_review')
 ]
